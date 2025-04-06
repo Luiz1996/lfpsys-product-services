742739d305f26bf52d7a710f347280d17079ce1d
Luiz1996 <luiz_flavio_p@hotmail.com>
2025-04-06 12:42:41 -0300

    Initial commit.

---
+++ src/main/java/com/lfpsys/lfpsys_product_services/LfpsysProductServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lfpsys.lfpsys_product_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpsysProductServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfpsysProductServicesApplication.class, args);
+	}
+
+}
